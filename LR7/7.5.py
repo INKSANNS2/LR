bf0286e6733760e216749fe1905a40367f11f572
INKSANNS2 <maks07200709@gmail.com>
2025-10-05 00:09:59 +0500

    Update 7.5.py

--- LR7/7.5.py
+++ LR7/7.5.py
@@ -1,45 +1,30 @@
 import math
-expression = input("Введите выражение (число знак число): ")
-parts = expression.split()
-if len(parts) != 3:
-    print("Ошибка: нужно ввести число, знак и число через пробел!")
+text = input("Введите числа и операцию через пробел: ")
+parts = text.split()
+
+if parts[1] == '/**':
+    num = float(parts[0])
+    result = math.sqrt(num)
 else:
-    try:
-        num1 = float(parts[0])
-        operator = parts[1]
-        num2 = float(parts[2])
-        if operator == '+':
-            result = num1 + num2
-        elif operator == '-':
-            result = num1 - num2
-        elif operator == '*':
-            result = num1 * num2
-        elif operator == '/':
-            if num2 != 0:
-                result = num1 / num2
-            else:
-                result = "Ошибка: деление на ноль!"
-        elif operator == '%':
-            if num2 != 0:
-                result = num1 % num2
-            else:
-                result = "Ошибка: деление на ноль!"
-        elif operator == '//':
-            if num2 != 0:
-                result = num1 // num2
-            else:
-                result = "Ошибка: деление на ноль!"
-        elif operator == '**':
-            result = num1 ** num2
-        elif operator == '%%':
-            result = (num2 / 100) * num1
-        elif operator == '/**':
-            if num1 >= 0:
-                result = math.sqrt(num1)
-            else:
-                result = "Ошибка: корень из отрицательного числа!"
-        else:
-            result = "Ошибка: неизвестная операция!"
-        print("Результат: ", result )
-    except ValueError:
-        print("Ошибка: введите числа корректно")
+    num1 = float(parts[0])
+    operator = parts[1]
+    num2 = float(parts[2])
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        result = num1 / num2
+    elif operator == '%':
+        result = num1 % num2
+    elif operator == '//':
+        result = num1 // num2
+    elif operator == '**':
+        result = num1 ** num2
+    elif operator == '%%':
+        result = num1 * num2 / 100
+    else:
+        result = "неизвестная операция"
+print("Ответ:", result)
