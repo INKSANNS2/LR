3ffb894d55bc1dfb060700a6fd1f12e31ed60a14
INKSANNS2 <maks07200709@gmail.com>
2025-12-22 12:02:10 +0500

    Update 1.2.py

--- 3 Deadline/LR1/1.2.py
+++ 3 Deadline/LR1/1.2.py
@@ -1,5 +1,4 @@
 def make_bold(func):
-    """Декоратор, оборачивающий результат в теги <b>"""
     def wrapper():
         return f"<b>{func()}</b>"
     return wrapper
@@ -7,4 +6,5 @@ def make_bold(func):
 @make_bold
 def get_text():
     return "Hello, World!"
+
 print(get_text()) 
